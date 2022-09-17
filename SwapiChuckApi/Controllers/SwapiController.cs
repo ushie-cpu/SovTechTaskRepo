46ea2a3fc32fa4530503217aa501a2fc9116f340
john.ushie_kuda <john.ushie_kuda@DESKTOP-M0OA61C>
2022-09-17 12:00:34 +0100

    Commit for Controllers

---
+++ SwapiChuckApi/Controllers/SwapiController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SwapiChuckApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SwapiController : ControllerBase
+    {
+        //// GET: api/<ChucksController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<ChucksController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<ChucksController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<ChucksController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ChucksController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
